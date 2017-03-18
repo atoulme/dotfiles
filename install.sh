bb37ea6bfe5467e5c40236c43c965963b2ac0495
Antoine Toulme <antoine@lunar-ocean.com>
2017-03-17 22:31:24 -0700

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -Rf ~/bin ~/.bash_profile ~/.gitconfig ~/.tcshrc
+mkdir -p ~/.bash.d
+touch ~/.bash.d/local
+ln -s `pwd`/bin ~/bin
+ln -s `pwd`/.bash_profile ~/.bash_profile 
+ln -s `pwd`/.gitconfig ~/.gitconfig 
+ln -s `pwd`/.tcshrc ~/.tcshrc 
