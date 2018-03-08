c211a36da738c572cbe39897a967b48947c5ba9e
Antoine Toulme <antoine@lunar-ocean.com>
2018-03-08 15:12:28 -0800

    Add a TextMate properties file so we save on loss of focus

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ touch ~/.bash.d/local
 ln -s `pwd`/bin ~/bin
 ln -s `pwd`/.bash_profile ~/.bash_profile 
 ln -s `pwd`/.gitconfig ~/.gitconfig 
-ln -s `pwd`/.tcshrc ~/.tcshrc 
+ln -s `pwd`/.tcshrc ~/.tcshrc
+ln -s `pwd`/.tm_properties ~/.tm_properties 
